931cf3ef6f381e653390e0dffe0da1568f005aa2
Sabbir Hossain <sabbirsopnil07@gmail.com>
2023-06-24 17:28:50 +0600

    Initial Commit

---
+++ SwiftUIMap/SwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp.swift
+//  SwiftUIMap
+//
+//  Created by Sopnil Sohan on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
